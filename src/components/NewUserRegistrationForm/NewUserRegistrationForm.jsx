cb232a05044166a048c700dae1a77c5735b1c3f7
Alex Severson <alexsvrsn@gmail.com>
2020-10-05 12:41:59 -0500

    added paper and centering to form for easier readability

--- src/components/NewUserRegistrationForm/NewUserRegistrationForm.jsx
+++ src/components/NewUserRegistrationForm/NewUserRegistrationForm.jsx
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 
+//MATERIAL-UI
+import {
+  Paper,
+  Grid,
+} from '@material-ui/core';
+
 import NewUserRegistrationName from './NewUserRegistrationName';
 import NewUserRegistrationContact from './NewUserRegistrationContact';
 import NewUserRegistrationPersonal from '../NewUserRegistrationForm/NewUserRegistrationPersonal';
+import { AutoComplete } from 'material-ui';
 
 
 class NewUserRegistrationForm extends Component {
@@ -15,10 +22,16 @@ class NewUserRegistrationForm extends Component {
   render() {
     return (
       <div>
-        <NewUserRegistrationName />
-        <NewUserRegistrationContact />
-        <NewUserRegistrationPersonal />
-
+        <Grid container justify="center" alignItems="center">
+          <Grid item xs={12} >
+            <Paper style={{ maxWidth: "80%", margin: "auto", padding: "20px"}}>
+              <h6 style={{ color: "red"}}>Fields with * are required.</h6>
+              <NewUserRegistrationName />
+              <NewUserRegistrationContact />
+              <NewUserRegistrationPersonal />
+            </Paper>
+          </Grid>
+        </Grid>
       </div>
     );
   }
