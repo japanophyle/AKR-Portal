3268f0e4e90e2ca9f2ac176817a2de1730b71151
Karl Beck <karljohnbeck@gmail.com>
2020-10-13 11:54:49 -0500

    dialog good to go, need to create the Saga for the shipping the new auth level

--- src/components/AuthSelect/AuthSelect.jsx
+++ src/components/AuthSelect/AuthSelect.jsx
@@ -1,24 +1,47 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
-import { MenuItem, Select, InputLabel, FormControl, Typography, Button, Card, CardContent, Grid, TextField, Tooltip, IconButton } from '@material-ui/core';
+import { Button,Dialog,DialogActions,DialogContent,DialogContentText,DialogTitle,MenuItem, Select, InputLabel, FormControl } from '@material-ui/core';
 
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
 // component name TemplateFunction with the name for the new component.
 function AuthSelect(props) {
-  // Using hooks we're creating local state for a "heading" variable with
-  // a default value of 'Functional Component'
-  const [heading, setHeading] = useState('Functional Component');
-  const [auth, setAuth] = React.useState(props.member.auth_level);
 
+    
+    // handles storing auth level locally fore the dispatch
+  const [auth, setAuth] = React.useState(props.member.auth_level);
+  // handles dialog open
+  const [open, setOpen] = React.useState(false);
+
+  // Open the dialog
+  const handleClickOpen = () => {
+    setOpen(true);
+};
+
+// close will not chnage the auth level ! 
+const handleClose = () => {
+    setOpen(false); 
+    setAuth(props.member.auth_level)
+};
+
+// SET UP THE DISPATCH HERE!!!!!!
+const handleSave = () => {
+    setOpen(false); 
+    console.log('WOOOOOO')
+    console.log(auth);
+}
+
+// Open the Dialog when you select a new level of auth
   const handleChange = (event) => {
     setAuth(event.target.value);
+    handleClickOpen()
   };
 
   return (
     <div>
+        {/* IF auth level of user is 20 you can promote/demote all */}
         {props.store.user.auth_level >= 20 && 
       <FormControl variant="outlined">
         <InputLabel id="demo-simple-select-outlined-label">Role</InputLabel>
@@ -41,6 +64,7 @@ function AuthSelect(props) {
       </FormControl>
           }
 
+{/* if you are auth level 10 you can only promote and demote people that are at or below your level */}
         {props.store.user.auth_level === 10 && 
         <>
             {props.member.auth_level >= 20 ? 
@@ -68,6 +92,29 @@ function AuthSelect(props) {
         }
         </>
         }
+
+{/* This is the dialog that opens when you try and change a users auth level for conformation */}
+<Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog"
+        aria-describedby={`are you sure you wish to promote/demote ${props.member.fname}`}
+      >
+        <DialogTitle id="alert-dialog-slide-title">{"Changing Authorization Level"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Are you sure you with the change the authorization level of {props.member.fname} {props.member.lname}?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Yes, I do!
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
