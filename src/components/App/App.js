b358671eb48c6ac0071a7a24e19c6076cf0c80b7
Alex Severson <alexsvrsn@gmail.com>
2020-10-21 16:08:49 -0500

    info page error fixed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,7 +18,6 @@ import SiteAdminAuthRoute from '../ProtectedRoute/SiteAdminAuthRoute';
 
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
