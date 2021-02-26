a0ffb8c7a1f9dbd500ba9e882dc1e18bbf07a6db
mbMosman <mbmosman@users.noreply.github.com>
2021-02-25 22:02:39 -0600

    typo in comment

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -22,7 +22,7 @@ if (process.env.DATABASE_URL) {
 // this creates the pool that will be shared by all other modules
 const pool = mysql.createPool(config);
 
-// the pool with emit an error on behalf of any idle clients
+// the pool will emit an error on behalf of any idle clients
 // it contains if a backend error or network partition happens
 pool.on('error', (err) => {
   console.log('Unexpected error on idle client', err);
